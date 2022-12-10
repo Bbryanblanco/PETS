6ade6f678a0e39b5ee77bfcde9cc89fdee67c780
bryanblanco <bernardbrylablanco@gmail.com>
2022-12-10 20:36:19 +0800

    Changing to HTML

--- session.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if($_SESSION["status"] == 'invalid' || empty($_SESSION["status"])){
-    $_SESSION["status"] == 'invalid';
-    unset($_SESSION["email"]);
-    echo '<script> window.location.href="index.php" </script>';
-};
